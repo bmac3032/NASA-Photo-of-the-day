afcc4dcdb901ce4cc647f35c00e434959ff1ab6b
Brian McPherson <heat3032@gmail.com>
2021-05-22 14:39:18 -0400

    typo fixes

--- src/Components/ImageOfDay.js
+++ src/Components/ImageOfDay.js
@@ -1,14 +1,13 @@
 import React from "react";
 
 const ImageOfDay = ({ images }) => {
-  console.log(images);
   return (
     <div className="card">
-      <p>Copyright: {images.copyright}</p>
-      <p>Date: {images.date}</p>
       <p>
-        <span className="underline">Title</span> <br /> {images.title}
+        Copyright: <em>{images.copyright}</em>
       </p>
+      <p>Date: {images.date}</p>
+      <p>Image Title: {images.title}</p>
       <img src={images.hdurl} alt={images.name} />
       <p className="img-explanation">{images.explanation}</p>
     </div>
--- src/Pages/About.js
+++ src/Pages/About.js
@@ -5,8 +5,12 @@ function About() {
   return (
     <div className="container">
       <h1>
-        This site was created with React, and uses functional componenets,
-        hooks, and also utilizes react router for client side routing
+        This site was created with <a href="https://reactjs.org/">React</a>, and
+        uses functional componenets, hooks, and also utilizes{" "}
+        <a href="https://reactrouter.com/web/guides/quick-start">
+          React Router
+        </a>{" "}
+        for client side routing
       </h1>
       <div className="underline"></div>
       <Link to="/">
